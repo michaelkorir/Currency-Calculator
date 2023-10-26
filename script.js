c07af26befbcb84db77cc174c3f02c074652c7f7
michael-korir <michaelkiptoo2000@gmail.com>
2023-10-26 10:41:58 +0300

    updated html

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
-document.addEventListener('DOMContentLoaded', (){
-    let apiURL=`https://v6.exchangerate-api.com/v6/a5b2065a7c003f8afdfb3bc3/latest/USD`;
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'a5b2065a7c003f8afdfb3bc3';
+    const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
 
     const amountInput = document.getElementById('amount');
-    const fromDropDown = document.getElementById("select-from-currency");
-    const toDropDown = document.getElementById("select-to-currency");
+    const fromCurrencySelect = document.getElementById('from');
+    const toCurrencySelect = document.getElementById('to');
     const convertButton = document.getElementById('convert');
     const resultDiv = document.getElementById('result');
 
-//fetch data from Exchangerate api 
-fetch(apiURL)
-    .then(res => res.json())
-    .then(data => {
-        //console.log(data);
-        const exchangeRates = data.convertion_rate;
-        const currencies =Object.keys(exchangeRates);
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const exchangeRates = data.conversion_rates;
+            const currencies = Object.keys(exchangeRates);
 
-        currencies.forEach(currency => {
-            const option = document.createElement('option');
-            option.value = currency;
-            option.textContent = currency;
-            fromDropDown.appendChild(option);
-            toDropDown.appendChild(option.cloneNode(true));
+            currencies.forEach(currency => {
+                const option = document.createElement('option');
+                option.value = currency;
+                option.textContent = currency;
+                fromCurrencySelect.appendChild(option);
+                toCurrencySelect.appendChild(option.cloneNode(true));
+            });
         })
-    })
-    .catch(error => {
-        console.error('Error fetching data:',error);
-    })
-    //Event Listener for currency conversion
-    convertButton.addEventListener('click',()=>{
-        const fromCurrency = fromDropDown.value;
-        const toCurrency =toDropDown.value
-        const amount =parseFloat(amountInput.value);
-        
-    })
+        .catch(error => {
+            console.error('Error fetching currency data:', error);
+        });
 
-}
+    convertButton.addEventListener('click', () => {
+        const fromCurrency = fromCurrencySelect.value;
+        const toCurrency = toCurrencySelect.value;
+        const amount = parseFloat(amountInput.value);
+
+        if (isNaN(amount)) {
+            resultDiv.textContent = 'Please enter a valid amount.';
+            return;
+        }
+
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const exchangeRates = data.conversion_rates;
+                const rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+                const convertedAmount = (amount * rate).toFixed(2);
+
+                resultDiv.textContent = `Result: ${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+            })
+            .catch(error => {
+                console.error('Error fetching exchange rates:', error);
+            });
+    });
+});
